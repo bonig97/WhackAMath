6d478d2aabf56a9eade39a12fc86e44c36a8e873
MagdElDinAhmed <magdeldin@aucegypt.edu>
2024-03-18 22:07:14 +0200

    Made moles appear and disappear
    Couldn't get tweens to work properly, the moles need a different sprite, and the clicking isn't working as intended, neither is the score

---
+++ MoleHouse.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class MoleHouse : Node
+{
+	private Label score_label;
+	private int score = 0;
+
+	// Called when the node enters the scene tree for the first time.
+
+	public override void _Ready()
+	{
+		score_label = GetNode<Label>("ScoreLabel");
+		score_label.Text = "Score: " + score;
+	}
+
+	public void _on_Mole_update_score()
+	{
+		score += 1;
+		score_label.Text = "Score: " + score;
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
