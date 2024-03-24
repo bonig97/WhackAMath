0c487071d55424a913e874a7346c4c916ab36007
Gabriele Boni <bonig97@gmail.com>
2024-03-24 19:26:44 +0100

    refactor Mole and MoleHouse classes: use PascalCase for variable names and use a single shared instance of Random

--- MoleHouse.cs
+++ MoleHouse.cs
@@ -3,28 +3,29 @@ using System;
 
 public partial class MoleHouse : Node
 {
-	private Label score_label;
+	private Label scoreLabel;
 	private int score = 0;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		score_label = GetNode<Label>("%ScoreLabel");
-		score_label.Text = "Score: " + score;
+		scoreLabel = GetNode<Label>("ScoreLabel");
+		scoreLabel.Text = "Score: " + score;
 
 		foreach (Node child in GetChildren())
 		{
 			if (child is Mole mole)
 			{
-				mole.Connect(nameof(Mole.UpdateScoreEventHandler), new Callable(this, nameof(_on_Mole_update_score)));
+				mole.Connect(nameof(Mole.UpdateScoreEventHandler), new Callable(this, nameof(_on_mole_update_score)));
 			}
 		}
 	}
 
-	public void _on_Mole_update_score()
+	private void _on_mole_update_score()
 	{
+		GD.Print("Score update signal received");
 		score += 1;
-		score_label.Text = "Score: " + score;
+		scoreLabel.Text = "Score: " + score;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
