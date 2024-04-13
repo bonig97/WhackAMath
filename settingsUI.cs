ab69c6817325d2190f982923090c850633590945
MagdElDinAhmed <magdeldin@aucegypt.edu>
2024-04-13 15:51:01 +0200

    Made settings work with C# and implemented the tests

--- settingsUI.cs
+++ settingsUI.cs
@@ -1,38 +1,53 @@
 using Godot;
 using System;
 
-public partial class MusicEffectsUI : Control
+public partial class settingsUI : Control
 {
-    private Button tutorialButton;
-    private HSlider musicSlider;
-    private HSlider effectsSlider;
+	private Button tutorialButton;
+	private HSlider musicSlider;
+	private HSlider effectsSlider;
+	private Button backButton;
 
-    public override void _Ready()
-    {
-        tutorialButton = GetNode<Button>("TutorialButton");
-        musicSlider = GetNode<HSlider>("HBoxContainer/MusicSlider");
-        effectsSlider = GetNode<HSlider>("HBoxContainer/EffectsSlider");
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		tutorialButton = GetNode<Button>("TutorialButton");
+		musicSlider = GetNode<HSlider>("VBoxContainer/Music/MusicSlider");
+		effectsSlider = GetNode<HSlider>("VBoxContainer/Effects/EffectsSlider");
+		backButton = GetNode<Button>("Backbutton");
+		
+		tutorialButton.Connect("pressed", new Callable(this,nameof(OnTutorialButtonPressed)));
+		musicSlider.Connect("value_changed", new Callable(this,nameof(OnMusicSliderValueChanged)));
+		effectsSlider.Connect("value_changed", new Callable(this,nameof(OnEffectsSliderValueChanged)));
+		backButton.Connect("pressed", new Callable(this,nameof(OnBackButtonPressed)));
+	}
 
-        tutorialButton.Pressed += OnTutorialButtonPressed;
-        // musicSlider.ValueChanged += OnMusicSliderValueChanged;
-        // effectsSlider.ValueChanged += OnEffectsSliderValueChanged;
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 
-    private void OnTutorialButtonPressed()
-    {
-        // Handle tutorial button press
-        GD.Print("Tutorial button pressed");
-    }
+	private void OnTutorialButtonPressed()
+	{
+		// Handle tutorial button press
+		GD.Print("Tutorial button pressed");
+	}
 
-    private void OnMusicSliderValueChanged(float value)
-    {
-        // Handle music slider value change
-        GD.Print($"Music volume: {value}");
-    }
+	private void OnMusicSliderValueChanged(float value)
+	{
+		// Handle music slider value change
+		GD.Print($"Music volume: {value}");
+	}
 
-    private void OnEffectsSliderValueChanged(float value)
-    {
-        // Handle effects slider value change
-        GD.Print($"Effects volume: {value}");
-    }
+	private void OnEffectsSliderValueChanged(float value)
+	{
+		// Handle effects slider value change
+		GD.Print($"Effects volume: {value}");
+	}
+
+	private void OnBackButtonPressed()
+	{
+		PackedScene mainUIScene = (PackedScene)ResourceLoader.Load("res://mainUI.tscn");
+		GetTree().ChangeSceneToPacked(mainUIScene);
+	}
 }
