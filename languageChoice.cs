6d8f03a2cfb021d15f24a5e0fdc3478cd4e02e42
MagdElDinAhmed <magdeldin@aucegypt.edu>
2024-04-13 17:31:37 +0200

    Added some buttons for languages

--- languageChoice.cs
+++ languageChoice.cs
@@ -5,9 +5,22 @@ public partial class languageChoice : Control
 {
 	
 	private Button backButton;
+	private Button englishButton;
+	private Button italianButton;
+	private Button arabicButton;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
+		englishButton = GetNode<Button>("EnglishButton");
+		englishButton.Connect("pressed", new Callable(this, nameof(OnEnglishButtonPressed)));
+
+		italianButton = GetNode<Button>("ItalianButton");
+		italianButton.Connect("pressed", new Callable(this, nameof(OnItalianButtonPressed)));
+
+		arabicButton = GetNode<Button>("ArabicButton");
+		arabicButton.Connect("pressed", new Callable(this, nameof(OnArabicButtonPressed)));
+
 		backButton = GetNode<Button>("BackButton");
 		backButton.Connect("pressed", new Callable(this, nameof(OnBackButtonPressed)));
 	}
@@ -22,5 +35,23 @@ public partial class languageChoice : Control
 		//change this to go back to previous scene via global variable
 		PackedScene loginScene = (PackedScene)ResourceLoader.Load("res://mainUI.tscn");
 		GetTree().ChangeSceneToPacked(loginScene);
+	}
+
+	private void OnEnglishButtonPressed()
+	{
+		//change this to go to english language
+		GD.Print("English Button Pressed");
+	}
+
+	private void OnItalianButtonPressed()
+	{
+		//change this to go to italian language
+		GD.Print("Italian Button Pressed");
+	}
+
+	private void OnArabicButtonPressed()
+	{
+		//change this to go to arabic language
+		GD.Print("Arabic Button Pressed");
 	}
 }
